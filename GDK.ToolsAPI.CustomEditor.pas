21352c004c3b5813312426caeb68bfa3f1198e0f
cdekraker <kdekraker@gdksoftware.nl>
2023-05-08 09:31:15 +0200

    Changed `TCustomEditorView` for supporting different Delphi versions

--- GDK.ToolsAPI.CustomEditor.pas
+++ GDK.ToolsAPI.CustomEditor.pas
@@ -9,26 +9,37 @@ uses
   Vcl.Graphics;
 
 type
+  {$IF CompilerVersion >= 35.0}
+  TCustomEditorView = class(TInterfacedObject, INTACustomEditorView, INTACustomEditorView150, INTACustomEditorView280)
+  {$ELSEIF CompilerVersion >= 22.0}
+  TCustomEditorView = class(TInterfacedObject, INTACustomEditorView, INTACustomEditorView150)
+  {$ELSE}
   TCustomEditorView = class(TInterfacedObject, INTACustomEditorView)
+  {$ENDIF}
   strict protected
     function GetCaption: string; virtual; abstract;
     function GetEditorWindowCaption: string; virtual; abstract;
     function GetViewIdentifier: string; virtual; abstract;
     function GetFrameClass: TCustomFrameClass; virtual; abstract;
     procedure FrameCreated(AFrame: TCustomFrame); virtual; abstract;
+
+    {$IF CompilerVersion >= 22.0}
     function GetImageIndex: Integer; virtual; abstract;
     function GetTabHintText: string; virtual; abstract;
+    procedure Close(var Allowed: Boolean); virtual;
+    {$ENDIF}
 
     function GetCanCloneView: Boolean; virtual;
     function CloneEditorView: INTACustomEditorView; virtual;
     function GetEditState: TEditState; virtual;
     function EditAction(Action: TEditAction): Boolean; virtual;
     procedure CloseAllCalled(var ShouldClose: Boolean); virtual;
-    procedure Close(var Allowed: Boolean); virtual;
     procedure SelectView; virtual;
     procedure DeselectView; virtual;
 
+    {$IF CompilerVersion >= 35.0}
     function GetTabColor: TColor; virtual;
+    {$ENDIF}
   public
     property CanCloneView: Boolean read GetCanCloneView;
     property Caption: string read GetCaption;
@@ -36,16 +47,6 @@ type
     property FrameClass: TCustomFrameClass read GetFrameClass;
     property ViewIdentifier: string read GetViewIdentifier;
   end;
-
-  {$IF CompilerVersion >= 22.0}
-  TCustomEditorView150 = class(TCustomEditorView, INTACustomEditorView150);
-  {$ENDIF}
-
-  {$IF CompilerVersion >= 35.0}
-  TCustomEditorView280 = class(TCustomEditorView150, INTACustomEditorView280);
-  {$ENDIF}
-
-
 
 implementation
 
@@ -86,14 +87,18 @@ begin
   // No default implementation
 end;
 
+{$IF CompilerVersion >= 22.0}
 procedure TCustomEditorView.Close(var Allowed: Boolean);
 begin
   Allowed := True;
 end;
+{$ENDIF}
 
+{$IF CompilerVersion >= 35.0}
 function TCustomEditorView.GetTabColor: TColor;
 begin
   Result := clNone;
 end;
+{$ENDIF}
 
 end.
